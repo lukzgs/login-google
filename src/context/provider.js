e6d23f3b3f80c160a57e591eb5a2c86373526701
Lucas G Silveira <lucgs88@gmail.com>
2022-10-04 16:45:15 -0300

    feat: added provider and context

---
+++ src/context/provider.js
@@ -0,0 +1,60 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { app } from "../services/firebase";
+import { Context } from "./context"; 
+
+export const Provider = ({ children }) => {
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storageContent = () => {
+      const sessionToken = sessionStorage.getItem('@AuthFirebase: token');
+      const sessionUser = sessionStorage.getItem('@AuthFirebase: user');
+      if (sessionToken && sessionUser) { setUser(sessionUser) };
+    };
+
+    storageContent();
+  }, []);
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      setUser(user);
+      sessionStorage.setItem('@AuthFirebase: token', token);
+      sessionStorage.setItem('@AuthFirebase: user', JSON.stringify(user));
+
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+    });
+  }
+
+  const signOut = () => {
+    sessionStorage.clear();
+    setUser(null);
+    return <Navigate to='/' />
+  }
+  
+  const value = {
+    user,
+    setUser,
+    signInWithGoogle,
+    signOut,
+    signed: !!user,
+  }
+
+  return (
+    <Context.Provider value={ value }>
+      { children }
+    </Context.Provider> 
+  )
+}
