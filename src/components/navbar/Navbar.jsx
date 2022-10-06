2d76d2eb29fc7b6a9780f11b33fb1bf07dcb967f
Lucas G Silveira <lucgs88@gmail.com>
2022-10-06 16:33:43 -0300

    feat: added name to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,15 @@
+import { useContext } from "react"
+import { Context } from "../../context/context"
 import './Navbar.css';
 
 export default function Navbar() {
+  const { user } = useContext(Context);
   return (
     <div className="side-by-side">
       <nav className="navbar">
         <ul>
           <li>
+            { user ? user.displayName : null }
           </li>
         </ul>
       </nav>
