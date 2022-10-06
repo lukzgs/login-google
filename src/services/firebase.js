b2f46fd60c5b846b9261398ab22f0dc5f315c07c
Lucas G Silveira <lucgs88@gmail.com>
2022-10-06 16:35:31 -0300

    fix: firestore added to services

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,10 +4,12 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/storage';
+import 'firebase/compat/firestore';
+
 import { firebaseConfig } from '../firebase';
 
 if (!firebase.apps.length) { firebase.initializeApp(firebaseConfig) };
 
 export const auth = firebase.auth()
-// export const firestore = firebase.firestore();
+export const firestore = firebase.firestore();
 export const storage = firebase.storage();
