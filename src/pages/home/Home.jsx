fd7e818f0a826a77d7574023571c3d3d18fb1a30
Lucas G Silveira <lucgs88@gmail.com>
2022-10-06 21:27:16 -0300

    fix: added choose username routes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,20 @@
 import { useContext } from "react"
+import { Navigate } from "react-router-dom";
 import { Context } from "../../context/context"
 import './Home.css'
 
 export const Home = () => {
-  const { user, signOut } = useContext(Context);
+  const { username, user, signOut } = useContext(Context);
   return (
     <>
       <main>
         <h1>Home</h1>
         <h3>Welcome { user.displayName }</h3>
+        { username ? null : <Navigate to='/username' /> }
         <button onClick={ () => signOut() }>
           Sign Out
         </button>
       </main>
-      
     </>
   )
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PrivateRoutes } from ".";
 import { Home } from "../pages/home/Home";
 import { Login } from "../pages/login/Login";
+import { ChooseUsername } from "../pages/username/Username";
 
 export const AppRoutes = () => {
   return (
@@ -10,6 +11,9 @@ export const AppRoutes = () => {
         <Route path='/' element={ <Login /> } />
         <Route path='/home' element={ <PrivateRoutes /> } >
           <Route path='/home' element={ <Home /> }/>
+        </Route>
+        <Route path='/username' element={ <PrivateRoutes /> } >
+          <Route path='/username' element={ <ChooseUsername /> }/>
         </Route>
       </Routes>
     </BrowserRouter>
