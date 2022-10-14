868d8c047e9ad0d23bd942c2f161b09cbee0a999
Lucas G Silveira <lucgs88@gmail.com>
2022-10-14 17:29:04 -0300

    rework: removed navbar from landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Navbar from "./components/navbar/Navbar"
-import { Provider } from "./context/provider"
-import { AppRoutes } from "./routes/routes"
+import { Provider } from "./context/provider";
+import { AppRoutes } from "./routes/routes";
 
 export const App = () => {
   return (
     <Provider>
-      <Navbar />
       <AppRoutes />
     </Provider>
   )
