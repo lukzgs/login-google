fcddd8dc71ca89e40584d61a966898897b4aecdd
Lucas G Silveira <lucgs88@gmail.com>
2022-10-21 18:46:12 -0300

    changes to match previous changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,14 +2,14 @@ import React, { useContext } from "react"
 import { Context } from "../../context/context"
 
 export const Home = () => {
-  const { user, signOut } = useContext(Context);
+  const { user, signingOut } = useContext(Context);
 
   return (
     <>
       <main>
         <h1>Home</h1>
         <h3>Welcome { user.displayName? user.displayName : user.email }</h3>
-        <button onClick={ () => signOut() }>
+        <button onClick={ () => signingOut() }>
           Sign Out
         </button>
       </main>
