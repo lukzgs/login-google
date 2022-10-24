b9d5cea035ec201bc7d246b7f880d7ed18014988
Lucas G Silveira <lucgs88@gmail.com>
2022-10-24 17:17:42 -0300

    feat: added confirm your email

---
+++ src/pages/confirmEmail/ConfirmEmail.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const ConfirmEmail = () => {
+
+  return (
+    <div className="confirm-page h-screen w-screen">
+      Please confirm your email 
+    </div>
+  )
+}
